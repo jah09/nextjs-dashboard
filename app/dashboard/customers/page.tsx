8c1516e2912ab8004c31a1fba6e85119af600287
jah09 <jasuhot1@gmail.com>
2024-02-08 22:36:48 +0800

    changes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+
+export default function page() {
+  return (
+    <div>Customer Page</div>
+  )
+}
+
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+
+export default function page() {
+  return (
+    <div className="text-xl font-bold">Invoice Page</div>
+  )
+}
+
